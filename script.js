f74ed9f88b6a3b9b8b909b3577892025b663a13b
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-03-15 15:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function minDate(dates) {
   //write you code here
+	    return dates.reduce((min, curr) => (new Date(curr) < new Date(min) ? curr : min));
+
 }
 
 // Do not change the code
@@ -37,4 +39,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+// alert(minDate(dates));
